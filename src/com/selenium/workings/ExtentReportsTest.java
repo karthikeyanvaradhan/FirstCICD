2bf68a2ed509a733ede0a6a6f8b772c0b62bc0e3
Karthikeyan Varadhan <VKarthikeyan@CEIIND.COM>
2021-11-30 08:40:43 +0530

    FirstCICD

---
+++ src/com/selenium/workings/ExtentReportsTest.java
@@ -0,0 +1,16 @@
+package com.selenium.workings;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ExtentReportsTest {
+	
+	public WebDriver browser(){
+		
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+		
+	}
+	
+
+}
