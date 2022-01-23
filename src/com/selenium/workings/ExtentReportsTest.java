f6d6299c8c240d1954b927bc70d353b2ae0666b3
karthikeyanvaradhan <karthee.test@gmail.com>
2022-01-23 21:06:11 +0530

    Test Fetch
    Test Fech

--- src/com/selenium/workings/ExtentReportsTest.java
+++ src/com/selenium/workings/ExtentReportsTest.java
@@ -3,6 +3,8 @@ package com.selenium.workings;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+# WebDriver Manager
+
 public class ExtentReportsTest {
 	
 	public WebDriver browser(){
